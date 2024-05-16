501b1807046751099310b3cb6b9b1edeea3a59d6
keuns0407 <kimeunsu0407@gmail.com>
2024-05-16 18:20:54 +0900

    EffectSound

--- MetaverseProject/Assets/Scripts/SoundController.cs
+++ MetaverseProject/Assets/Scripts/SoundController.cs
@@ -18,13 +18,14 @@ public class SoundController : MonoBehaviour
     void Start()
     {
         BS_Slider.value = PlayerPrefs.GetFloat("BGM", 0.75f);
+        ES_Slider.value = PlayerPrefs.GetFloat("EffectSound", 0.75f);
     }
 
     void Awake()
     {
         instance = this;
-       // bgm_player = GameObject.Find("BGM").GetComponent<AudioSource>();
-       // efs_player = GameObject.Find("EffectSound").GetComponent<AudioSource>();
+        bgm_player = GameObject.Find("BGM").GetComponent<AudioSource>();
+        efs_player = GameObject.Find("EffectSound").GetComponent<AudioSource>();
 
         BS_Slider.onValueChanged.AddListener(ChangeBgmSound); //슬라이더로 BGM조절
         ES_Slider.onValueChanged.AddListener(ChangeEfsSound); //슬라이더로 효과음조절
@@ -40,6 +41,8 @@ public class SoundController : MonoBehaviour
             case "Boost" : index = 0; break;
             case "Crash" : index = 1; break;
             case "Drift" : index = 2; break;
+            case "Engine" : index = 3; break;
+            case "RaceStart" : index = 4; break;
         }
 
         efs_player.clip = audio_clips[index];
@@ -49,9 +52,13 @@ public class SoundController : MonoBehaviour
     public void ChangeBgmSound(float volume) //슬라이더 BGM 볼륨설정
     {
         mixer.SetFloat("BGM", Mathf.Log10(volume)* 20);
+        PlayerPrefs.SetFloat("BGM", volume);
     }
     public void ChangeEfsSound(float volume) //슬라이더 효과음 볼륨설정
     {
         mixer.SetFloat("EffectSound", Mathf.Log10(volume)* 20);
+        efs_player.Play(); //소리변경 시 효과음 한 번 재생
+        PlayerPrefs.SetFloat("EffectSound", volume);
+
     }
 }
