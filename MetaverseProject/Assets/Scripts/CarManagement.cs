b8e84140646cafad72670c62b0c43a62cca0208f
alsrudursla <nuy0307@naver.com>
2024-05-19 15:34:56 +0900

    대기실에서 게임 맵으로 전환 시 태그한 차 생성

--- MetaverseProject/Assets/Scripts/CarManagement.cs
+++ MetaverseProject/Assets/Scripts/CarManagement.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 
 public class CarManagement : MonoBehaviour
 {
-    public string selectedCarTag; //대기실에서 선택된 자동차의 태그
+    public string selectedCarTag; //선택된 차량의 태그 저장
 
-    // Start is called before the first frame update
     void Start()
     {
+        //PlayerPrefs에서 선택된 차량의 태그를 가져옴
         selectedCarTag = PlayerPrefs.GetString("selectedCarTag");
-        
-        GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
-        //모든 자동차 GameObject 배열 반복
-        foreach (GameObject car in cars)
-        {
-            //현재 자동차의 태그가 선택된 자동차의 태그와 일치하는지 확인
-            if (car.CompareTag(selectedCarTag))
-            {
-                //선택된 자동차->활성화
-                car.SetActive(true);
-            }
-            else
-            {
-                //선택되지 않음->비활성화
-                car.SetActive(false);
-            }
-        }
-        
+
+        //모든 차량 숨기는 함수
+        HideAllCars();
+
+        //선택된 차량 보이는 함수
+        ShowSelectedCar();
     }
 
-    // Update is called once per frame
-    void Update()
+    void HideAllCars()
     {
-        
+        //"Car" 태그를 가진 모든 차량을 배열로 가져옴
+        GameObject[] cars = GameObject.FindGameObjectsWithTag("Car");
+
+        //배열의 각 차량에 대해 반복
+        foreach (GameObject car in cars)
+        {
+            //차량 비활성화
+            car.SetActive(false);
+        }
+    }
+
+    void ShowSelectedCar()
+    {
+        //선택된 차량의 태그 가진 게임 오브젝트 가져옴
+        GameObject selectedCar = GameObject.FindGameObjectWithTag(selectedCarTag);
+
+        //선택된 차량이 존재하는 경우
+        if (selectedCar != null)
+        {
+            //선택된 차량 활성화
+            selectedCar.SetActive(true);
+        }
     }
 }
+
